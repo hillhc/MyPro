6a5158fd2dc88e70ed3977b84974a5e63a47337a
hchoy <hill6e08@gmail.com>
2024-02-21 22:57:54 +0900

    New ine

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -12,9 +12,9 @@ function Project() {
         <p>
           The project involves the use of the latest object detection model, YOLOv8, with MobileNetV3 backbone structure, to design and train a
           model capable of detecting the presence of vehicles in parking spaces. This model can be deployed on the edge devices with limited
-          computational power (Raspberry Pi) to capture and determine if a parking slot is empty or occupied. The project also involves the design
-          and building of backend system structure and the use of LoRaWAN technology to transmit the parking space availability information to
-          the backend, which can then be used to inform drivers of available parking spaces. The project is currently in the development stage.
+          computational power (Raspberry Pi) to capture and determine if a parking slot is empty or occupied.<br />
+          The project also involves the design and building of backend system structure and the use of LoRaWAN technology to transmit the parking space availability information to
+          the backend, which can then be used to inform drivers of available parking spaces. The project is currently in the development stage.<br />
           More details about the project can be found at: <a href="http://eek123.ust.hk/SmartCarpark">http://eek123.ust.hk/SmartCarpark</a>
         </p>
       </Collapsible1>

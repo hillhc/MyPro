a79d3a1eb47c54ac06989c42cd96df7e0cac4e64
hillhc <hill6e08@gmail.com>
2024-02-21 11:18:34 +0800

    line update

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -5,13 +5,14 @@ import Collapsible1 from "../resource/project1"
 function Project() {
   return(
     <div className="Project">
-      <hr />
     <h1>Final Year Project</h1>
+    <hr />
     <Collapsible1 label="Final Year Project">
       <p>
         This will contain the details of my final year project. Update will be done later.
       </p>
     </Collapsible1>
+    <hr />
     </div>
   )
 }

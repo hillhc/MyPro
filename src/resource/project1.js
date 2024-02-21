b15c8e0b88f952cc2bb44808cf27b3b6fd9fb340
hillhc <hill6e08@gmail.com>
2024-02-21 11:15:43 +0800

    Material Button

--- src/resource/project1.js
+++ src/resource/project1.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
+import Button from '@mui/material/Button';
 
 const Collapsible1 = (props) => {
   const [open, setOpen] = useState(false);

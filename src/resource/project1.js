bb4540177c7d14ecb50bbc8a33fad1d88e320b2d
hillhc <hill6e08@gmail.com>
2024-02-21 10:56:27 +0800

    Trial3

--- src/resource/project1.js
+++ src/resource/project1.js
@@ -2,10 +2,17 @@ import React, { useState } from 'react';
 
 const Collapsible1 = () => {
   const [open, setOpen] = useState(false);
+  const toggle = () => {
+    setOpen(!open);
+  };
   return (
     <div>
-      <button>toggle</button>
-      {open && <div>toggle me</div>}
+      <button onClick={toggle}>toggle</button>
+      {open && (
+        <div className="toggle">
+          <h4>toggle</h4>
+        </div>
+      )}
     </div>
   );
 };

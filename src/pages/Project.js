431f93b53b22693d5eb354d80c5e459504c2a02c
hillhc <hill6e08@gmail.com>
2024-03-01 16:09:40 +0800

    Project Update

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -32,6 +32,15 @@ function Project() {
                 The project also involves the design and building of a backend system structure and the use of LoRaWAN technology to transmit parking space availability information to
                 the backend, which can then be used to inform drivers of available parking spaces.
               </Typography>
+              <Typography variant="subtitle1" style={{ textAlign: "justify", fontSize: "1.2rem", marginBottom: "1rem" }}>
+                Current Progress
+              </Typography>
+              <Typography variant="body1" style={{ textAlign: "justify", fontSize: "1.2rem", marginBottom: "1rem" }}>
+                In the current stage, the project has completed the training of the YOLOv8 model and the deployment of the model on a Raspberry Pi. The application can pass the captured vacancy information to the backend AWS server via the LoRaWAN network. The backend server then processes and merges the information from different edge devices and stores it in the database.
+                The project frontend is currently under development and will be used to display the parking space availability information to the users, the basic version of the frontend can
+                be found here: <a href="https://fyp-react-control-panel.pages.dev/" style={{ textDecoration: "none", color: "blue" }}>https://fyp-react-control-panel.pages.dev/</a>. 
+                The model utilized in the project is also undergoing fine-tuning to improve performance and versatility.
+              </Typography>
               <img src={projectImage} alt="Project Image" style={{ width: "50%", height: "auto", objectFit: "cover", marginBottom: "1rem" }} />
               <Typography variant="body1" style={{ textAlign: "justify", fontSize: "1.2rem", marginBottom: "1rem" }}>
                 The project is currently in the development stage. More details about the project can be found at: 
